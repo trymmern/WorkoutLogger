919c19d6cbf81d57bc9d138c0e05d51610a733a6
Trym Røsand Todalshaug <trym.rosand.todalshaug@nhn.no>
2020-12-17 15:23:04 +0100

    initial commit with project created

---
+++ WorkoutLogger/WorkoutLogger/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using WorkoutLogger.ViewModels;
+using WorkoutLogger.Views;
+using Xamarin.Forms;
+
+namespace WorkoutLogger
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ WorkoutLogger/WorkoutLogger/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using WorkoutLogger.ViewModels;
+
+namespace WorkoutLogger.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
